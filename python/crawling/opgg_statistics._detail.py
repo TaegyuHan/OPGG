1be8a21e7bd5f3d5a73ee9a81a1f5599f0f3c157
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-06-22 23:20:04 +0900

    챔피언 detail 페이지 진행중

--- python/crawling/opgg_statistics._detail.py
+++ python/crawling/opgg_statistics._detail.py
@@ -7,7 +7,7 @@
 """
 
 
-
+import re
 from opgg_statistics import OpggStatistics
 
 
@@ -116,3 +116,153 @@ class OpggStatisticsDetail(OpggStatistics):
 
         except:
             self.logger.error("FUC | OpggStatisticsDetail.champion_line_url  > ")
+
+
+
+
+    def champion_detail_synthesize(self):
+
+        self.logger.info("FUC | OpggStatisticsDetail.champion_detail_synthesize > run")
+
+        # 결과 딕셔너리
+        result_dict = {}
+
+        html = self.read_html("https://www.op.gg/champion/aatrox/statistics/top")
+        tbody = html.find_all("tbody")
+
+        # 카운터 챔피언
+        counter_champion = tbody[0].find_all("td", class_="champion-stats-header-matchup__table__champion")
+        counter_winrate = tbody[0].find_all("td", class_="champion-stats-header-matchup__table__winrate")
+
+        result_dict["CounterChampion"] = {}
+
+        for i in range(len(counter_champion)):
+          cc = counter_champion[i].get_text().strip() # 카운터 챔피언 
+          cw = counter_winrate[i].find("b").get_text().replace("%", "") # 카운터 챔피언  승률
+          result_dict["CounterChampion"][cc] = cw
+
+
+        # 상대하기 쉬운 챔피언
+        counter_champion = tbody[1].find_all("td", class_="champion-stats-header-matchup__table__champion")
+        counter_winrate = tbody[1].find_all("td", class_="champion-stats-header-matchup__table__winrate")
+
+        result_dict["EasyChampion"] = {}
+
+        for i in range(len(counter_champion)):
+          cc = counter_champion[i].get_text().strip() # 카운터 챔피언 
+          cw = counter_winrate[i].find("b").get_text().replace("%", "") # 카운터 챔피언  승률
+          result_dict["EasyChampion"][cc] = cw
+
+
+        # 챔피언 스펠
+        result_dict["Spell"] = {}
+        result_dict["Spell"][0] = {}
+        result_dict["Spell"][1] = {}
+        result_dict["Spell"][0]["SpellImage"] = []
+        result_dict["Spell"][1]["SpellImage"] = []
+
+        # 챔피언 spell 이미지 추출
+        spell_list = tbody[2].find_all("li", class_="champion-stats__list__item")
+
+        # 이미지 정규식
+        pattern = "([-\w]+\.(?:jpg|gif|png|jpeg))"
+
+        for i in range(len(spell_list)):
+            spell_image = re.search(pattern, spell_list[i].img["src"]).group()
+            if i//2 == 0:
+                result_dict["Spell"][0]["SpellImage"].append(spell_image)
+            else:
+                result_dict["Spell"][1]["SpellImage"].append(spell_image)
+        
+        # spell 픽률
+        spell_pick_list = tbody[2].find_all("td", class_="champion-overview__stats champion-overview__stats--pick")
+
+        for i in range(len(spell_pick_list)):
+            result_dict["Spell"][i]["PickPercentage"] = \
+              spell_pick_list[i].find("strong").get_text().replace("%", "")
+
+            result_dict["Spell"][i]["PickCount"] = \
+              spell_pick_list[i].find("span").get_text()
+
+
+        # spell 승률
+        spell_win_list = tbody[2].find_all("td", class_="champion-overview__stats champion-overview__stats--win")
+        for i in range(len(spell_win_list)):
+            result_dict["Spell"][i]["WinRate"] = \
+              spell_win_list[i].find("strong").get_text().replace("%", "")
+
+        # 챔피언 스킬
+        result_dict["SkillBuild"] = []
+
+        skill_list = tbody[4].find_all("td")
+        for i in range(len(skill_list)):
+            skill = skill_list[i].get_text().strip()
+            result_dict["SkillBuild"].append(skill)
+
+        # 챔피언 게임시작 아이템
+        result_dict["ItemBuild"] = {}
+        result_dict["ItemBuild"]["StarterItems"] = {}
+        result_dict["ItemBuild"]["RecommendedBuilds"] = {}
+        result_dict["ItemBuild"]["Boots"] = {}
+
+        item_list = tbody[5].find_all("tr")
+        for i in range(len(item_list)):
+
+            il = [] # 아이템 이미지 저장 리스트 
+
+            if i in range(1): # 시작 아이템
+                dict_tmp_name = "StarterItems"
+            if i in range(2, 6): # 추천 빌드
+                dict_tmp_name = "RecommendedBuilds"
+            if i in range(7, 9): # 신발
+                dict_tmp_name = "Boots"
+                
+            result_dict["ItemBuild"][dict_tmp_name][i] = {}
+
+            for tag in item_list[i].find_all("img"): # 이미지 리스트에 넣기
+                item_image = re.search(pattern, tag["src"]).group()
+                # blet.png 이미지 제거
+                if item_image == "blet.png": continue
+                il.append(item_image)
+
+            result_dict["ItemBuild"][dict_tmp_name][i]["Images"] = il # 이미지 리스트
+
+            # 픽률
+            result_dict["ItemBuild"][dict_tmp_name][i]["PickPercentage"] = \
+              item_list[i].find("strong").get_text().replace("%", "")
+
+            # 픽 횟수
+            result_dict["ItemBuild"][dict_tmp_name][i]["PickCount"] = \
+              item_list[i].find("span").get_text()
+
+            # 픽 승률
+            result_dict["ItemBuild"][dict_tmp_name][i]["WinRate"] = \
+              item_list[i].find_all("strong")[1].get_text()
+
+
+        self.html_code_save(
+          item_list[2],
+          "champion_detail_synthesize.txt"
+        )
+
+        # 룬 헤더
+        tbody[6]
+
+        # 첫번째 룬 정보
+        tbody[7]
+
+        # 두번째 룬 정보
+        tbody[8]
+
+        # 트렌드 그래프
+        html.find("div", class_="champion-box-content")
+
+        return result_dict
+
+
+
+if __name__ == '__main__':
+    a = OpggStatisticsDetail()
+    print(
+      a.champion_detail_synthesize()
+    )
