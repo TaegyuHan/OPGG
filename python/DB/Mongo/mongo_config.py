ee27a24bc970116cea568341625fbef2b6db9f29
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-06-24 18:01:01 +0900

    Mongo DB 연결 code

---
+++ python/DB/Mongo/mongo_config.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+""" 
+    MongoDB 연결 core code
+
+"""
+
+import pymongo
+
+import sys
+sys.path.append("../../")
+from log import log # 로그
+
+
+
+
+class MongoDB_DB():
+
+
+
+
+    def __init__(self, collection_name):
+        """MongoDB 연결
+
+        Args:
+            collection_name ([string]): collection 이름 입력
+                ex ) : "customers"
+
+        Returns:
+            [class]: <class 'pymongo.cursor.Cursor'>
+        """
+
+        # 로그 생성
+        self.logger = log.make_logger("MongoDB_DB")
+        self.logger.info("CLASS | MongoDB_DB > run")
+
+        # 접속 경로
+        client = "mongodb://localhost:27017/"
+        database = "opgg"
+        collection = collection_name
+
+        try:
+            self.client = pymongo.MongoClient(client)
+            self.db = self.client[daabase] # Database
+            self.cursor = self.db[collection] # Collection
+            self.logger.info("CLASS | MySQL_DB > success")
+
+        except:
+            self.logger.error("CLASS | DB connect | > error")
+
+
+
