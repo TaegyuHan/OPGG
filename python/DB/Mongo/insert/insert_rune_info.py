d737c94c355764ae1e1d1b3baced02ed69c9d5a7
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-06-24 21:05:46 +0900

    Create insert_rune_info.py

---
+++ python/DB/Mongo/insert/insert_rune_info.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+"""
+   DB : MongoDB
+   Database : opgg
+   Collection : itemInfo
+
+   데이터 입력 code
+"""
+
+import sys
+import os
+import json
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from mongo_config import MongoDB_DB
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+from log import log # 로그
+
+
+def insert_rune_info(collection):
+
+    logger = log.make_logger("MongoDB_DB")
+    logger.info("FUC | insert_item_info > run")
+
+    try:
+
+        json_file_path = "C:\github\OPGG\json\ko_KR\\runesReforged.json"
+
+        # JSON file
+        f = open (json_file_path, "r", encoding='UTF8')
+
+        # Reading from file
+        json_data = json.loads(f.read())
+        # Iterating through the json
+
+        print(json_data)
+
+        # Closing file
+        f.close()
+
+        # DB 연결
+        MongoDB = MongoDB_DB(collection)
+
+        # 리스트 DB insert
+        cursor = MongoDB.connect()
+        cursor.insert_many(json_data)
+    
+    except:
+        logger.error("FUC | insert_item_info > error")
+
+
+if __name__ == '__main__':
+
+    collection = "runeReforgedInfo"
+    insert_rune_info(collection)
