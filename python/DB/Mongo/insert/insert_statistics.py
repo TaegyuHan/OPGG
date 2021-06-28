75a97a21a87fff27beabd092881a7b154fcc4094
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-06-28 10:13:05 +0900

    insert_statistics 함수 2개 생성
    OPGG 통계 데이터 insert

---
+++ python/DB/Mongo/insert/insert_statistics.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+"""
+   DB : MongoDB
+   Database : opgg
+   Collection : championInfochampionStatisticsInfo 
+   데이터 입력 code
+"""
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from mongo_config import MongoDB_DB
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+from log import log # 로그
+from DB.PreprocessData.preprocess_statistics import PreprocessStatistics
+
+
+
+
+def insert_statistics(collection):
+    """ 크롤링한 데이터를 전처리 후
+        DB에 넣습니다.
+
+        preprocess_func :
+            prepro_champion_statistics_info()
+
+    """
+
+    logger = log.make_logger("MongoDB_DB")
+    logger.info("FUC | {} > run".format(sys._getframe().f_code.co_name))
+
+    try:
+        PS = PreprocessStatistics()
+        list_data = PS.prepro_champion_statistics_info()
+
+        # Iterating through the json
+        print(list_data)
+
+        # # DB 연결
+        MongoDB = MongoDB_DB(collection)
+
+        # # 리스트 DB insert
+        cursor = MongoDB.connect()
+        cursor.insert_many(list_data)
+    
+    except:
+        logger.error("FUC | {} > error".format(sys._getframe().f_code.co_name))
+
+
+
+
+def insert_statistics_ban(collection):
+    """ 크롤링한 데이터를 전처리 후
+        DB에 넣습니다.
+
+        preprocess_func :
+            prepro_champion_statistics_ban_info()
+            
+    """
+
+    logger = log.make_logger("MongoDB_DB")
+    logger.info("FUC | {} > run".format(sys._getframe().f_code.co_name))
+
+    try:
+        PS = PreprocessStatistics()
+        list_data = PS.prepro_champion_statistics_ban_info()
+
+        # Iterating through the json
+        print(list_data)
+
+        # # DB 연결
+        MongoDB = MongoDB_DB(collection)
+
+        # # 리스트 DB insert
+        cursor = MongoDB.connect()
+        cursor.insert_many(list_data)
+    
+    except:
+        logger.error("FUC | {} > error".format(sys._getframe().f_code.co_name))
+
+
+
+
+# if __name__ == '__main__':
+
+#     collection = "championStatisticsInfo"
+#     insert_statistics(collection)
+
+#     collection = "championStatisticsBanInfo"
+#     insert_statistics_ban(collection)
