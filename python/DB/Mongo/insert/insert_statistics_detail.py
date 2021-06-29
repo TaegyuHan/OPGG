8931b83065c2aeaec2a8357c0df9f1c2c94e7b01
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-06-29 17:31:05 +0900

    champ_num_line 함수 생성
    챔피언 번호와 라인을 반환하는 함수 따로 생성

---
+++ python/DB/Mongo/insert/insert_statistics_detail.py
@@ -0,0 +1,73 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+from log import log # 로그
+
+
+def champ_num_line(url, cursor_find):
+    """URL을 받아서 챔피언의 번호를
+       반환 하는 함수
+
+    Args:
+        url ([string]): 챔피언 검색 URL
+              ex ) : https://www.op.gg/champion/aatrox/statistics/top/
+
+        cursor_find ([class]): Mongo DB 연결 
+              type : <class 'pymongo.collection.Collection'>
+
+    Returns:
+        [string]: champ_number > 챔피언 번호 
+                    ex ) : 266
+                     
+                  champ_line > 챔피언 가는 라인
+                    ex ) : TOP
+
+    """
+
+    logger = log.make_logger("MongoDB_DB")
+    logger.info("FUC | {} > run".format(sys._getframe().f_code.co_name))
+
+    try:
+        champ_line = url.split("/")[-1].upper()
+        champ_name = url.split("/")[-3
+        ].capitalize().replace("'", "").replace(".", "").replace(" ", "")
+
+        champ_name_change = {
+            "Aurelionsol":"AurelionSol",
+            "Drmundo":"DrMundo",
+            "Jarvaniv":"JarvanIV",
+            "Kogmaw":"KogMaw",
+            "Leesin":"LeeSin",
+            "Masteryi":"MasterYi",
+            "Missfortune":"MissFortune",
+            "Reksai":"RekSai",
+            "Tahmkench":"TahmKench",
+            "Twistedfate":"TwistedFate",
+            "Wukong" : "WuKong", # 오공 json 데이터 없음
+            "Xinzhao":"XinZhao",
+        }
+        
+        # 챔피언 이름 중간 소문자 대문자로 변경
+        for key, val in champ_name_change.items():
+            if key == champ_name:
+                champ_name = val
+
+        # 챔피언 번호 추출
+        for dict in cursor_find.find({"champName":champ_name}):
+            champ_number = dict["data"][champ_name]["key"]
+
+        # 예외 처리 오공 json 데이터 없음
+        # 따라서 직접 넣어줌
+        if champ_name == "WuKong":
+            champ_number = 62
+        
+
+        return champ_number, champ_line
+
+    except:
+        self.logger.error("FUC | {} > error".format(sys._getframe().f_code.co_name))
+
+
+
+        
--- python/DB/Mongo/insert/insert_statistics_detail_skill.py
+++ python/DB/Mongo/insert/insert_statistics_detail_skill.py
@@ -11,7 +11,9 @@
 import sys
 import os
 import json
-from time import sleep
+
+# 챔피언 이름 라인 함수 호출
+from insert_statistics_detail import champ_num_line
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from mongo_config import MongoDB_DB # DB class
@@ -21,8 +23,6 @@ from log import log # 로그
 
 from crawling.opgg_statistics_detail_skill \
   import OpggStatisticsDetailSkill # 전처리 class
-
-
 
 
 def insert_statistics_detail_skill(collection):
@@ -36,53 +36,26 @@ def insert_statistics_detail_skill(collection):
     logger = log.make_logger("MongoDB_DB")
     logger.info("FUC | {} > run".format(sys._getframe().f_code.co_name))
 
-
+    # 챔피언 번호 find DB 연결
     MongoDB_champ_info = MongoDB_DB("championInfo")
-    MongoDB = MongoDB_DB(collection)
-
-    cursor_insert = MongoDB.connect()
     cursor_find = MongoDB_champ_info.connect()
 
+    # 챔피언 skill 정보 insert DB 연결
+    MongoDB = MongoDB_DB(collection)
+    cursor_insert = MongoDB.connect()
 
     CRAWLING = OpggStatisticsDetailSkill() # 크롤링 연결
     url_list = CRAWLING.champion_line_url() # URL 가져오기
 
+    print(type(cursor_find))
 
     # 챔피언 URL을 가지고
     # 이름과 라인 추출
     for url in url_list:
-        champ_line = url.split("/")[-1].upper()
-        champ_name = url.split("/")[-3
-        ].capitalize().replace("'", "").replace(".", "").replace(" ", "")
 
-        champ_name_change = {
-            "Aurelionsol":"AurelionSol",
-            "Drmundo":"DrMundo",
-            "Jarvaniv":"JarvanIV",
-            "Kogmaw":"KogMaw",
-            "Leesin":"LeeSin",
-            "Masteryi":"MasterYi",
-            "Missfortune":"MissFortune",
-            "Reksai":"RekSai",
-            "Tahmkench":"TahmKench",
-            "Twistedfate":"TwistedFate",
-            "Wukong" : "WuKong", # 오공 json 데이터 없음
-            "Xinzhao":"XinZhao",
-        }
-        
-        # 챔피언 이름 중간 소문자 대문자로 변경
-        for key, val in champ_name_change.items():
-            if key == champ_name:
-                champ_name = val
-
-        # 챔피언 번호 추출
-        for dict in cursor_find.find({"champName":champ_name}):
-            champ_number = dict["data"][champ_name]["key"]
-
-        # 예외 처리 오공 json 데이터 없음
-        # 따라서 직접 넣어줌
-        if champ_name == "WuKong":
-            champ_number = 62
+        # 챔피언 라인 번호 호출 함수
+        champ_number, champ_line = \
+            champ_num_line(url, cursor_find)
 
         # data 가져오기
         data = CRAWLING.champion_detail_skill(url, champ_number, champ_line)
@@ -98,5 +71,5 @@ def insert_statistics_detail_skill(collection):
 
 if __name__ == '__main__':
 
-    collection = "championDetailSkill"
+    collection = "championDetailSkill2"
     insert_statistics_detail_skill(collection)
