a30d2f051f11fb384e05daf0658cc40cdc990c61
hantaegyu <59700057+hantaegyu@users.noreply.github.com>
2021-06-29 18:58:57 +0900

    함수 예외 처리 제거

--- python/DB/Mongo/insert/insert_statistics_detail.py
+++ python/DB/Mongo/insert/insert_statistics_detail.py
@@ -1,4 +1,3 @@
-import sys
 
 
 def champ_num_line(url, cursor_find):
@@ -21,48 +20,42 @@ def champ_num_line(url, cursor_find):
 
     """
 
-    logger = log.make_logger("MongoDB_DB")
-    logger.info("FUC | {} > run".format(sys._getframe().f_code.co_name))
+    champ_line = url.split("/")[-1].upper()
+    champ_name = url.split("/")[-3
+    ].capitalize().replace("'", "").replace(".", "").replace(" ", "")
 
-    try:
-        champ_line = url.split("/")[-1].upper()
-        champ_name = url.split("/")[-3
-        ].capitalize().replace("'", "").replace(".", "").replace(" ", "")
+    champ_name_change = {
+        "Aurelionsol":"AurelionSol",
+        "Drmundo":"DrMundo",
+        "Jarvaniv":"JarvanIV",
+        "Kogmaw":"KogMaw",
+        "Leesin":"LeeSin",
+        "Masteryi":"MasterYi",
+        "Missfortune":"MissFortune",
+        "Reksai":"RekSai",
+        "Tahmkench":"TahmKench",
+        "Twistedfate":"TwistedFate",
+        "Wukong" : "WuKong", # 오공 json 데이터 없음
+        "Xinzhao":"XinZhao",
+    }
+    
+    # 챔피언 이름 중간 소문자 대문자로 변경
+    for key, val in champ_name_change.items():
+        if key == champ_name:
+            champ_name = val
 
-        champ_name_change = {
-            "Aurelionsol":"AurelionSol",
-            "Drmundo":"DrMundo",
-            "Jarvaniv":"JarvanIV",
-            "Kogmaw":"KogMaw",
-            "Leesin":"LeeSin",
-            "Masteryi":"MasterYi",
-            "Missfortune":"MissFortune",
-            "Reksai":"RekSai",
-            "Tahmkench":"TahmKench",
-            "Twistedfate":"TwistedFate",
-            "Wukong" : "WuKong", # 오공 json 데이터 없음
-            "Xinzhao":"XinZhao",
-        }
-        
-        # 챔피언 이름 중간 소문자 대문자로 변경
-        for key, val in champ_name_change.items():
-            if key == champ_name:
-                champ_name = val
+    # 챔피언 번호 추출
+    for dict in cursor_find.find({"champName":champ_name}):
+        champ_number = dict["data"][champ_name]["key"]
 
-        # 챔피언 번호 추출
-        for dict in cursor_find.find({"champName":champ_name}):
-            champ_number = dict["data"][champ_name]["key"]
+    # 예외 처리 오공 json 데이터 없음
+    # 따라서 직접 넣어줌
+    if champ_name == "WuKong":
+        champ_number = 62
+    
 
-        # 예외 처리 오공 json 데이터 없음
-        # 따라서 직접 넣어줌
-        if champ_name == "WuKong":
-            champ_number = 62
-        
+    return champ_number, champ_line
 
-        return champ_number, champ_line
-
-    except:
-        self.logger.error("FUC | {} > error".format(sys._getframe().f_code.co_name))
 
 
 
